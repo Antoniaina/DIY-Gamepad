b60734d2c2ed30f74fedbb7f27d3414adddb814d
RAMAHALISON Antoniaina <arisehenoantoniaina@gmail.com>
2025-11-30 23:20:12 +0300

    chore: reorganize firmware and bridge code into modulat structure

---
+++ bridge/src/packet.rs
@@ -0,0 +1,44 @@
+#[repr(C, packed)]
+#[derive(Debug)]
+pub struct Packet {
+    header: u8,
+    j1x: u16,
+    j1y: u16,
+    j1b: u8,
+    j2x: u16,
+    j2y: u16,
+    j2b: u8,
+    checksum: u8,
+}
+
+
+impl Packet {
+    pub const SIZE: usize = 12;
+
+    pub fn parse(buf: &[u8]) -> Option<Self> {
+        if (buf.len() != Self::SIZE) || (buf[0] != 0xAAu8) { return None; }
+
+        let checksum: u8 = buf[..Self::SIZE - 1]
+            .iter()
+            .fold(0u16, |acc, &b| acc + b as u16) as u8;
+
+        if checksum != buf[Self::SIZE - 1] {
+            eprintln!(
+                "Checksum error: got {:02X}, expected {:02X}",
+                buf[Self::SIZE - 1], checksum
+            );
+            return None;
+        }
+
+        Some(Packet {
+            header: buf[0],
+            j1x: u16::from_le_bytes([buf[1], buf[2]]),
+            j1y: u16::from_le_bytes([buf[3], buf[4]]),
+            j1b: buf[5],
+            j2x: u16::from_le_bytes([buf[6], buf[7]]),
+            j2y: u16::from_le_bytes([buf[8], buf[9]]),
+            j2b: buf[10],
+            checksum: buf[11],
+        })
+    }
+}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "joystick.h"
+#include "protocol.h"
 
 #define J1_X A1
 #define J1_Y A0
@@ -9,40 +10,9 @@
 #define J2_Y A2
 #define J2_BTN 3
 
-struct __attribute__((packed)) Packet {
-  uint8_t header;
-  uint16_t j1x;
-  uint16_t j1y;
-  uint8_t j1b;
-  uint16_t j2x;
-  uint16_t j2y;
-  uint8_t j2b;
-  uint8_t checksum;
-};
 
 Joystick joy1(J1_X, J1_Y, J1_BTN);
 Joystick joy2(J2_X, J2_Y, J2_BTN);
-
-void sendPacket() {
-  Packet payload;
-
-  payload.header = 0xAA;
-  payload.j1x = joy1.getX();
-  payload.j1y = joy1.getY();
-  payload.j1b = joy1.isPressed() ? 1 : 0;
-  payload.j2x = joy2.getX();
-  payload.j2y = joy2.getY();
-  payload.j2b = joy2.isPressed() ? 1 : 0;
-
-  uint8_t *raw = (uint8_t *)&payload;
-
-  uint8_t sum = 0;
-  for (int i=0; i< sizeof(Packet) -1; i++) sum += raw[i];
-
-  payload.checksum = sum;
-
-  Serial.write(raw, sizeof(Packet));
-}
 
 void setup() {
   Serial.begin(115200);
@@ -53,6 +23,6 @@ void setup() {
 void loop() {
   joy1.update();
   joy2.update();
-  sendPacket();
+  sendPacket(joy1, joy2);
   delay(200);
 }
